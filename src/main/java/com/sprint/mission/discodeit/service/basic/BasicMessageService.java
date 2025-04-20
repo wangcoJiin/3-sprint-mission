d366ec00519db6aae44aebcd8038b50015841772
wangcoJiin <mf0d070208@gmail.com>
2025-04-20 22:18:45 +0900

    add: Basic 서비스 구현체 생성

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -0,0 +1,221 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class BasicMessageService implements MessageService {
+
+    private static final String FILE_PATH = "message.ser";
+
+    // 의존성
+    private final ChannelService channelService;
+    private final UserService userService;
+    private final MessageRepository messageRepository;
+
+    public BasicMessageService(UserService userService, ChannelService channelService, MessageRepository messageRepository) {
+        this.userService = userService;
+        this.channelService = channelService;
+        this.messageRepository = messageRepository;
+    }
+
+    //메시지 생성
+    @Override
+    public Message CreateMessage(UUID channelId, String password, UUID senderId, String messageContent) {
+        Channel channel = channelService.getChannelUsingId(channelId);
+        User user = userService.getUserById(senderId);
+
+        if(isUserExist(user) &&
+                isChannelExist(channel) &&
+                isParticipant(channel, senderId) &&
+                isChannelLock(channel, password)) {
+
+            System.out.println("채널에 입장하셨습니다.");
+
+            Message newMessage = new Message(channelId, senderId, messageContent);
+            messageRepository.createMessage(newMessage);
+
+            System.out.println("메시지가 생성됐습니다.");
+
+            return newMessage;
+        }
+        return null;
+    }
+
+    // 메시지 수정
+    @Override
+    public boolean updateMessage(UUID channelId, String password, UUID messageId, UUID senderId, String newMessageContent) {
+        Channel channel = channelService.getChannelUsingId(channelId);
+        User user = userService.getUserById(senderId);
+
+        Message message = messageRepository.findMessageById(messageId);
+
+        if (isUserExist(user) &&
+                isChannelExist(channel) &&
+                isParticipant(channel, senderId) &&
+                isChannelLock(channel, password) &&
+                isSender(message, senderId)) {
+            System.out.println("채널에 입장하셨습니다.");
+
+            messageRepository.updateMessage(messageId, newMessageContent);
+
+            System.out.println("메시지가 수정되었습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    // 전체 메세지 조회
+    @Override
+    public List<Message> getAllMessage() {
+        return messageRepository.findAllMessage();
+    }
+
+    // 채널 메시지 조회
+    @Override
+    public List<Message> getMessageByChannel(UUID channelId, UUID userId, String password) {
+        Channel channel = channelService.getChannelUsingId(channelId);
+        User user = userService.getUserById(userId);
+
+        if (isUserExist(user) &&
+                isChannelExist(channel) &&
+                isParticipant(channel, userId) &&
+                isChannelLock(channel, password)) {
+            System.out.println("채널에 입장하셨습니다.");
+
+            return messageRepository.findMessageByChannel(channelId);
+        }
+        return null;
+    }
+
+    // 메시지 아이디 이용한 조회
+    @Override
+    public Message getMessageById(UUID channelId, UUID userId, String password, UUID messageId) {
+        Channel channel = channelService.getChannelUsingId(channelId);
+        User user = userService.getUserById(userId);
+
+        Message message = messageRepository.findMessageById(messageId);
+
+
+        if (isMessageExist(message) &&
+                isUserExist(user) &&
+                isChannelExist(channel) &&
+                isParticipant(channel, userId) &&
+                isChannelLock(channel, password)) {
+            return message;
+
+        }
+        return null;
+    }
+
+    // 발송자를 이용해서 조회
+    @Override
+    public List<Message> userMessage(UUID senderId, String password) {
+        User user = userService.getUserById(senderId);
+
+        List<Channel> foundChannel = channelService.getAllChannels().stream()
+                .filter(channel -> channel.getJoiningUsers().contains(senderId))
+                .collect(Collectors.toList());
+
+        for(Channel channel : foundChannel){
+            if (isUserExist(user) &&
+                    isChannelExist(channel) &&
+                    isChannelLock(channel, password)) {
+                System.out.println("확인되었습니다.");
+            }
+        }
+        return messageRepository.userMessage(senderId);
+    }
+
+    // 메시지 삭제
+    @Override
+    public boolean deletedMessage(UUID messageId, UUID senderId, String password) {
+        Message message = messageRepository.findMessageById(messageId);
+
+        User user = userService.getUserById(senderId);
+
+        if (isMessageExist(message) &&
+                isUserExist(user) &&
+                isChannelExist(channelService.getChannelUsingId(message.getChannelId())) &&
+                isParticipant(channelService.getChannelUsingId(message.getChannelId()), senderId) &&
+                isChannelLock(channelService.getChannelUsingId(message.getChannelId()), password) &&
+                isSender(message, senderId)){
+            System.out.println("확인되었습니다.");
+
+            messageRepository.deletedMessage(messageId);
+
+            System.out.println("메시지가 삭제되었습니다.");
+            return true;
+        }
+        return false;
+    }
+
+
+    /* 자주 쓰는 조건문 정리 */
+
+    //유저 존재 검사
+    private boolean isUserExist(User user){
+        if (user == null){
+            System.out.println("해당하는 유저가 존재하지 않습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    //채널 존재 검사
+    private boolean isChannelExist(Channel channel){
+        if (channel == null){
+            System.out.println("해당하는 채널이 존재하지 않습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    //참여자 여부 검사
+    private boolean isParticipant(Channel channel, UUID senderId){
+        if(!channel.getJoiningUsers().contains(senderId)){
+            System.out.println("해당 채널에 참여하고 있지 않습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    //채널 비밀번호 대조
+    private boolean isChannelLock(Channel channel, String password){
+        if (channel.isLock()) {
+            System.out.println("비공개 채널입니다. 비밀번호를 확인하고 있습니다..");
+            if (!channel.getPassword().equals(password)) {
+                System.out.println("비밀번호가 일치하지 않습니다.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //메시지 존재 검사
+    private boolean isMessageExist(Message message){
+        if(message == null){
+            System.out.println("해당하는 메시지가 없습니다.");
+            return false;
+        }
+        return true;
+    }
+
+    //메시지 발송자 대조
+    private boolean isSender(Message message, UUID senderId){
+        if (!message.getSenderId().equals(senderId)){
+            System.out.println("본인이 보낸 메시지만 수정할 수 있습니다.");
+            return false;
+        }
+        return true;
+    }
+
+}
+
---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -0,0 +1,137 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BasicUserService implements UserService {
+
+    //레포지토리 의존성
+    private final UserRepository userRepository;
+    public BasicUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private static final Logger logger = Logger.getLogger(BasicUserService.class.getName());
+
+    // 유저 생성
+    @Override
+    public User createUser(String name) {
+        System.out.println("유저 생성중");
+        User newUser = new User(name, "초기값");
+        userRepository.saveUser(newUser);
+        return newUser;
+    }
+
+    // 기존 유저 리스트에 새로운 유저 추가
+    @Override
+    public void addUserToRepository(User user) {
+        userRepository.saveUser(user);
+    }
+
+    // 유저 아이디 이용해서 조회
+    @Override
+    public User getUserById(UUID id) {
+
+        return userRepository.findUserById(id);
+    }
+
+    // 유저 이름 이용해서 조회
+    @Override
+    public List<User> searchUsersByName(String name) {
+
+        return userRepository.findUserByName(name);
+    }
+
+    // 전체 유저 정보 조회
+    @Override
+    public List<User> getAllUsers() {
+
+        return userRepository.findUserAll();
+    }
+
+    // 유저 이름과 활동상태 둘 다 변경
+    @Override
+    public boolean updateUser(UUID id, String name, String connectState) {
+        User user = userRepository.findUserById(id);
+        if (user != null) {
+            userRepository.updateUserName(user, name);
+            userRepository.updateConnectState(user, connectState);
+            System.out.println("유저의 이름, 활동상태가 수정됐습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    // 유저 이름 변경
+    @Override
+    public boolean updateUserName(UUID id, String newName) {
+        User user = userRepository.findUserById(id);
+        if (user != null) {
+            userRepository.updateUserName(user, newName);
+            System.out.println("유저 이름이 변경되었습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    // 유저 활동상태 변경
+    @Override
+    public boolean updateConnectState(String name, String connectState) {
+        List<User> users = userRepository.findUserByName(name);
+
+        if (users.isEmpty()) {
+            System.out.println("조회된 유저가 없습니다.");
+            return false;
+        }
+
+        if (users.size() == 1) {
+            System.out.println("활동상태 수정중");
+            userRepository.updateConnectState(users.get(0), connectState);
+            return true;
+        }
+        System.out.println("조회된 유저가 두 명 이상입니다.");
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            System.out.println("[" + i + "]" + " 생성 시간: " + user.getCreatedAt() + ", 수정 시간: " + user.getUpdatedAt() + ", 활동 상태: " + user.getConnectState());
+        }
+
+        System.out.print("수정을 원하는 유저의 번호를 입력해주세요.\n");
+        try (Scanner scanner = new Scanner(System.in)) {
+            int selection = scanner.nextInt();
+
+            if (selection >= 0 && selection < users.size()) {
+                User selectedUser = users.get(selection);
+                userRepository.updateConnectState(selectedUser, connectState);
+                System.out.println("선택한 유저의 활동 상태가 변경되었습니다.");
+                logger.info("선택한 유저의 활동 상태 변경");
+                return true;
+            } else {
+                logger.warning("잘못된 번호입니다.");
+                return false;
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "입력 처리 중 오류 발생", e);
+            return false;
+        }
+    }
+
+    // 유저 삭제
+    @Override
+    public boolean deleteUserById(UUID id) {
+        User user = userRepository.findUserById(id);
+        if (user == null) {
+            System.out.println("조회된 유저가 없습니다.");
+            return false;
+        }
+        userRepository.deleteUser(id);
+        System.out.println("유저가 삭제되었습니다.");
+        return true;
+    }
+
+}
+
