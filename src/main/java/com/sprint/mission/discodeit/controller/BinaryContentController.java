341a52a78743a43f8b7725385c1c64a3c0a331a3
wangcoJiin <mf0d070208@gmail.com>
2025-05-21 00:58:39 +0900

    modify: api-docs에서 요구하는 BinaryContentController 스펙에 맞게 RESTful api로 수정

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,36 +1,69 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.ReadStatus;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "BinaryContent", description = "첨부 파일 API")
 @RequiredArgsConstructor
-@RequestMapping("/api/binaryContent")
-@Controller
+@RequestMapping("/api/binaryContents")
+@RestController
 public class BinaryContentController {
 
     private final BinaryContentService binaryContentService;
 
-    @RequestMapping(value ="/find", method = RequestMethod.GET)
-    public ResponseEntity<BinaryContent> find(@RequestParam("binaryContentId") UUID binaryContentId) {
+    @GetMapping("/{binaryContentId}")
+    @Operation(summary = "첨부 파일 조회")
+    @ApiResponses(
+        value = {
+                @ApiResponse(responseCode = "200", description = "첨부 파일 조회 성공",
+                    content = @Content(schema = @Schema(implementation = BinaryContent.class))),
+                @ApiResponse(responseCode = "404", description = "첨부 파일을 찾을 수 없음",
+                    content = @Content(examples = @ExampleObject(value = "BinaryContent with id {binaryContentId} not found")))
+        }
+    )
+    public ResponseEntity<BinaryContent> find(
+            @Parameter(description = "조회할 첨부파일 ID")
+            @PathVariable("binaryContentId") UUID binaryContentId
+    ) {
         BinaryContent binaryContent = binaryContentService.findBinaryContentById(binaryContentId);
         return ResponseEntity.ok(binaryContent);
     }
 
     // BinaryContent 다중 조회
-    @RequestMapping(value ="/findAll", method = RequestMethod.GET)
-    public ResponseEntity<List<BinaryContent>> findAll(
+    @GetMapping
+    @Operation(summary = "여러 첨부 파일 조회")
+    @ApiResponses(
+        value = {
+                @ApiResponse(responseCode = "200", description = "첨부 파일 목록 조회 성공",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = BinaryContent.class))))
+        }
+    )
+    public ResponseEntity<List<BinaryContent>> findAllByIdIn(
+            @Parameter(description = "조회할 첨부 파일 ID 목록")
             @RequestParam("binaryContentIds") List<UUID> binaryContentIds) {
         List<BinaryContent> binaryContents = binaryContentService.findAllBinaryContent(binaryContentIds);
-        return ResponseEntity.ok(binaryContents);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(binaryContents);
     }
 }
