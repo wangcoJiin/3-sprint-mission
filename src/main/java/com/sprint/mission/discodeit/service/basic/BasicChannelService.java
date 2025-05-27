aa2ae3ce0a3e90935d278b418f1a3f03e2aa9c81
wangcoJiin <mf0d070208@gmail.com>
2025-05-27 13:09:57 +0900

    modify: ReadStatus 생성 시 채널 생성 시간으로 초기 시간값 지정

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -67,7 +67,7 @@ public class BasicChannelService implements ChannelService {
         Channel createdChannel = channelRepository.save(channel);
 
         request.participantIds().stream()
-                .map(userId -> new ReadStatus(userId, createdChannel.getId(), Instant.MIN))
+                .map(userId -> new ReadStatus(userId, createdChannel.getId(), channel.getCreatedAt()))
                 .forEach(readStatusRepository::save);
 
         return createdChannel;
