51603599441a2c550bce1c84c6cc16a3a3a26e49
wangcoJiin <mf0d070208@gmail.com>
2025-06-08 22:08:40 +0900

    refactor: 시간 자동 생성을 위한 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplicationTest.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplicationTest.java
@@ -3,7 +3,9 @@ package com.sprint.mission.discodeit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class DiscodeitApplicationTest {
 
