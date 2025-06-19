da2b659aad07b9027cd5c2bbe813a04a2b9061f7
wangcoJiin <mf0d070208@gmail.com>
2025-06-19 18:23:52 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ChannelRepository.java
@@ -1,19 +1,14 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.Channel;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, UUID> {
 
     // 중복 검사
     boolean existsByName(String channelName);
-
-    // 채널 이름으로 조회
-    Optional<Channel> findChannelByName(String channelName);
 
 }
--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.custom.CustomUserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, CustomUserRepository {
 
     // 중복 검사
     boolean existsByEmail(String email);
@@ -16,8 +17,5 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     // 유저 조회 (이름)
     Optional<User> findByUsername(String userName);
-
-    // 유저 조회 (이메일)
-    Optional<User> findUserByEmail(String email);
 
 }
