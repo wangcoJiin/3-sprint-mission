08d4960d5b227e9210685de1403f9db2af864105
wangcoJiin <mf0d070208@gmail.com>
2025-06-19 17:47:32 +0900

    refactor: 메서드 반환값 타입을 DTO로 수정

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -1,23 +1,25 @@
 package com.sprint.mission.discodeit.service;
+
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
 import com.sprint.mission.discodeit.dto.request.MessageUpdateRequest;
 import com.sprint.mission.discodeit.dto.response.MessageDto;
-import com.sprint.mission.discodeit.entity.Message;
-
+import com.sprint.mission.discodeit.dto.response.PageResponse;
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 
 public interface MessageService {
 
     // 메시지 생성
     MessageDto create(MessageCreateRequest request, List<BinaryContentCreateRequest> binaryContentCreateRequests);
 
-    // 채널의 메시지 조회
-    List<Message> findAllByChannelId(UUID channelId);
+    // 채널 메시지 조회
+    PageResponse<MessageDto> getAllByChannelId(UUID channelId, Instant cursor, Pageable pageable);
 
     // 채널의 특정 메시지 조회
-    Message find(UUID messageId);
+    MessageDto find(UUID messageId);
 
     // 메시지 수정
     MessageDto update(UUID messageId, MessageUpdateRequest request);
