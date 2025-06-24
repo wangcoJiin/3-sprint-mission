da3c16318e32d7ca6d37b19e23f1f535d1e1fa06
wangcoJiin <mf0d070208@gmail.com>
2025-06-24 10:30:26 +0900

    feat: 커스텀 예외 전역 처리 기능 추가

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,17 +1,16 @@
 package com.sprint.mission.discodeit.exception;
 
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+@Slf4j
 @RestControllerAdvice
-
 public class GlobalExceptionHandler {
 
     private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
@@ -40,11 +39,23 @@ public class GlobalExceptionHandler {
                 .body("널포인터 예외가 발생했습니다.: " + ex.getMessage());
     }
 
+    @ExceptionHandler(DiscodeitException.class)
+    public ResponseEntity<ErrorResponse> handleDiscodeitExcetion(DiscodeitException e){
+        log.error("[GlobalExceptionHandler] DiscodeitException 발생: {}", e.getMessage(), e);
+
+        int status = e.getErrorCode().getStatus();
+        ErrorResponse errorResponse = ErrorResponse.of(e);
+
+        return ResponseEntity
+                .status(status)
+                .body(errorResponse);
+    }
+
     // defalt 예외 처리기
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResponseEntity<String> handleException(Exception ex) {
-        logger.log(Level.SEVERE, "서버 내부 오류 발생", ex);
+        log.error("[GlobalExceptionHandler] 서버 내부 오류 발생", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 내부 오류: " + ex.getMessage());
     }
