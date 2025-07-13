5ffa54c7f3243f9f75941463e2d9c0b58b30dbf7
wangcoJiin <mf0d070208@gmail.com>
2025-07-13 22:08:36 +0900

    refactor: @NotNull 위치 수정

--- src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PrivateChannelCreateRequest.java
@@ -11,5 +11,5 @@ public record PrivateChannelCreateRequest(
         @NotNull(message = "참여자 목록은 필수입니다.")
         @Size(min = 2, message = "참여자는 2명 이상이어야 합니다.")
         @Valid
-        List<@NotNull(message = "참여자 ID는 null일 수 없습니다.") UUID> participantIds
+        List<UUID> participantIds
 ){ }
