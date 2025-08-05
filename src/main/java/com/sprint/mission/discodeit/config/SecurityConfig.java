f23aaf849ff491c27291947ceccd1e26043d0d41
wangcoJiin <mf0d070208@gmail.com>
2025-08-05 16:31:54 +0900

    config: SecurityConfig에 CSRF 보호 설정 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.sprint.mission.discodeit.config;
+
+import java.util.List;
+import java.util.stream.IntStream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    /* 설명. 필터 체인 디버깅을 위한 Bean 설정
+     *  CommandLineRunner를 사용해 애플리케이션 시작 시 필터 체인의 클래스 이름을 출력하여 디버깅 용도로 사용한다.
+     * */
+    @Bean
+    public CommandLineRunner debugFilterChain(SecurityFilterChain filterChain) {
+
+        return args -> {
+            int filterSize = filterChain.getFilters().size();
+
+            List<String> filterNames = IntStream.range(0, filterSize)
+                    .mapToObj(idx -> String.format("\t[%s/%s] %s", idx + 1, filterSize,
+                            filterChain.getFilters().get(idx).getClass()))
+                    .toList();
+
+            System.out.println("현재 적용된 필터 체인 목록:");
+            filterNames.forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
+            );
+
+        return http.build();
+    }
+}
