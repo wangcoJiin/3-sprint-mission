b8eb9479b428253d312352497cb2cccfb07abbb7
wangcoJiin <mf0d070208@gmail.com>
2025-08-07 14:46:10 +0900

    refactor: DiscodeitUserDetailsService 로직 수정

--- src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetailsService.java
+++ src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetailsService.java
@@ -1,14 +1,17 @@
 package com.sprint.mission.discodeit.auth.service;
 
 import com.sprint.mission.discodeit.dto.response.UserDto;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.mapper.UserMapper;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DiscodeitUserDetailsService implements UserDetailsService {
@@ -18,12 +21,11 @@ public class DiscodeitUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findByUsername(username)
-            .map(user -> {
-                UserDto userDto = userMapper.toDto(user);
-                return new DiscodeitUserDetails(userDto, user.getPassword());
-            })
-            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username)
-        );
+
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException(username));
+        UserDto userDto = userMapper.toDto(user);
+
+        return new DiscodeitUserDetails(userDto, user.getPassword());
     }
 }
