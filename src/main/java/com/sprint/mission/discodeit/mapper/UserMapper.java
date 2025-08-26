a7c8c76ee2c47019267214c8b613e2487c0e7f3d
wangcoJiin <mf0d070208@gmail.com>
2025-08-26 18:26:33 +0900

    refactor: 모범답안에 맞게 UserMapper 수정

--- src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/UserMapper.java
@@ -1,16 +1,21 @@
 package com.sprint.mission.discodeit.mapper;
 
+import com.sprint.mission.discodeit.auth.jwt.registry.JwtRegistry;
 import com.sprint.mission.discodeit.dto.response.UserDto;
 import com.sprint.mission.discodeit.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring", uses = BinaryContentMapper.class)
-public interface UserMapper {
+public abstract class UserMapper {
+
+    @Autowired
+    protected JwtRegistry jwtRegistry;
 
     @Mapping(target = "online", source = "online")
-    UserDto toDto(User user, Boolean online);
+    public abstract UserDto toDto(User user, Boolean online);
 
     @Mapping(target = "online", expression = "java(jwtRegistry.hasActiveJwtInformationByUserId(user.getId()))")
-    UserDto toDto(User user);
+    public abstract UserDto toDto(User user);
 }
