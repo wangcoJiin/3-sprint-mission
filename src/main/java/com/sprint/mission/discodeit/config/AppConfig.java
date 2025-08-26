6c8a59fdd343bc08d9de03f83c93b0f5a685d15f
wangcoJiin <mf0d070208@gmail.com>
2025-08-26 18:29:12 +0900

    add: 스케줄링 위한 AppConfig 추가

---
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class AppConfig {
+
+}
