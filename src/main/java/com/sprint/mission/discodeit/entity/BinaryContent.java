a9cd14b2846892c2b3dcb6bff51ef236c921885d
wangcoJiin <mf0d070208@gmail.com>
2025-08-31 01:40:37 +0900

    refactor: BinaryContent 엔티티에 status 필드 추가, Enum 클래스 추가

--- src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.entity;
 
-import com.sprint.mission.discodeit.entity.base.BaseEntity;
+import com.sprint.mission.discodeit.entity.base.BaseUpdatableEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "binary_contents")
 @Getter
-public class BinaryContent extends BaseEntity {
+public class BinaryContent extends BaseUpdatableEntity {
 
     @Column(name = "file_name", nullable = false)
     private String fileName;
@@ -23,11 +25,19 @@ public class BinaryContent extends BaseEntity {
     @Column(name = "content_type", nullable = false)
     private String contentType;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private BinaryContentStatus status = BinaryContentStatus.PROCESSING;
+
 
     public BinaryContent(String fileName, Long size, String contentType) {
         this.fileName = fileName;
         this.size = size;
         this.contentType = contentType;
+    }
+
+    public void updateStatus(BinaryContentStatus status) {
+        this.status = status;
     }
 
     @Override
