88e9a5273e1e5ca77683cc16d6fb2b0f7f2dacf6
wangcoJiin <mf0d070208@gmail.com>
2025-08-31 01:41:53 +0900

    refactor: 바이너리 데이터 저장 성공 여부를 메타 데이터에 반영

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
@@ -1,6 +1,8 @@
 package com.sprint.mission.discodeit.event.listener;
 
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
 import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class BinaryContentEventListener {
 
     private final BinaryContentStorage binaryContentStorage;
+    private final BinaryContentService binaryContentService;
 
     // 이벤트를 발행한 메인 서비스의 트랜잭션이 커밋되었을 때 리스너가 실행
     @Async("fileTaskExecutor")
@@ -28,10 +31,12 @@ public class BinaryContentEventListener {
         log.info("[BinaryContentEventListener] BinaryContentStorage에 저장 중");
         try {
             binaryContentStorage.put(fileId, bytes);
+            binaryContentService.updateStatus(fileId, BinaryContentStatus.SUCCESS);
             log.debug("[BinaryContentEventListener] 저장 성공 - 바이너리 파일 id={}", fileId);
 
 
         } catch (Exception e) {
+            binaryContentService.updateStatus(fileId, BinaryContentStatus.FAIL);
             log.error("[BinaryContentEventListener] 저장 실패 - 바이너리 파일 id={}", fileId, e);
         }
     }
