b5e07133f513482f8cc383a23df823716120bcfd
wangcoJiin <mf0d070208@gmail.com>
2025-09-03 15:44:05 +0900

    refactor: @EnableRetry 어노테이션을 활용해 Spring Retry를 활성화

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@EnableRetry
 public class AppConfig {
 
 }
