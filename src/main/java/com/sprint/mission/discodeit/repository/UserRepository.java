13abc7ae3b6a0707db482a632734aad5a62c0f8b
wangcoJiin <mf0d070208@gmail.com>
2025-09-03 18:37:19 +0900

    feat: Role에 해당하는 유저 목록 조회하는 JPA 메서드 추가

--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -3,11 +3,11 @@ package com.sprint.mission.discodeit.repository;
 import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.custom.CustomUserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID>, CustomUserRepository {
@@ -20,5 +20,7 @@ public interface UserRepository extends JpaRepository<User, UUID>, CustomUserRep
     Optional<User> findByUsername(String userName);
 
     boolean existsByRole(Role role);
+
+    List<User> findAllByRole(Role role);
 
 }
