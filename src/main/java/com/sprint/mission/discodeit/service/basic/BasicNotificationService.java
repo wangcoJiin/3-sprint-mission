5328733e223ca8c334e410ae16e40f81fe52d66f
wangcoJiin <mf0d070208@gmail.com>
2025-09-04 00:59:03 +0900

    feat: 사용자별 알림 목록 조회 메서드에 @ Cacheable 추가

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,6 +30,7 @@ public class BasicNotificationService implements NotificationService {
 
     @Override
     @Transactional(readOnly = true)
+    @Cacheable(value = "notificationsByUser", key = "#receiverId")
     public List<NotificationDto> findAllNotifications(UUID receiverId) {
        return notificationRepository.findAllByReceiverId(receiverId)
            .stream()
