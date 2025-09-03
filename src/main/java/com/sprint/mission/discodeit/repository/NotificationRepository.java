91e956ea194ea984236b65e6b79477848aaab30c
wangcoJiin <mf0d070208@gmail.com>
2025-09-03 15:38:26 +0900

    add: NotificationRepository 추가

---
+++ src/main/java/com/sprint/mission/discodeit/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Notification;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    List<Notification> findAllByReceiverId(UUID receiverId);
+
+    void deleteById(UUID id);
+}
